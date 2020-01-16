d764b449d652f7f495bd4cff01fe027c43829303
younespyco <younespyco@gmail.com>
2020-01-16 19:45:37 +0100

    ajout de lettre pas a sa place

--- game.c
+++ game.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "game.h"
 
-void comparer(char input[5],char mot[5],char *evaluation)
+void comparer(char input[5],char mot[5],char evaluation[5])
 {
     for(int i=0;i<4;i++)
     {
@@ -10,5 +11,21 @@ void comparer(char input[5],char mot[5],char *evaluation)
             evaluation[i] = 'V';
         else
             evaluation[i] = 'F';
+    }
+    char tab[5];
+    strcpy(tab,evaluation);
+    for(int i=0;i<4;i++)
+    {
+        if(evaluation[i] == 'F')
+        {
+            for(int j=0;j<4;j++)
+            {
+                if(tab[j] == 'F' && input[i] == mot[j])
+                {
+                    evaluation[i] = 'P';
+                    tab[j] = 'F';
+                }
+            }
+        }
     }
 }
